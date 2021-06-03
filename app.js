f9b1967a5d38dfc9d1bbe2c7724cb7223e93ca35
Javier Martinez Ribo <javiermribo@gmail.com>
2021-06-03 18:29:27 -0300

    problem fixed with input validation

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const FORM_SELECTOR = document.querySelector('#formInputs');
 successNewsletterListener();
 
 function successNewsletterListener() {
-  NEWSLETTER_BTN_SELECTOR.addEventListener('submit', successNewsletterMsg);
+  NEWSLETTER_BTN_SELECTOR.addEventListener('click', successNewsletterMsg);
 }
 
 function successNewsletterMsg(event) {
   event.preventDefault();
   const FORM_INPUT_NAME = document.querySelector('#formInputName').value;
   const FORM_INPUT_EMAIL = document.querySelector('#formInputEmail').value;
-  if(FORM_INPUT_NAME.length && FORM_INPUT_EMAIL.length <= 0) {
-    errorMsgBanner();
+  if(FORM_INPUT_NAME.length >= 0 && FORM_INPUT_EMAIL.length >= 0) {
+    errorNewsletterMsg();
     return;
   }
   const NEWSLETTER_SECTION_SELECTOR = document.querySelector('#newsletterSectionId');
@@ -31,18 +31,29 @@ const SUBSCRIPTION_BTN_SELECTOR = document.querySelector("#cat-domination-plan")
 subscriptionBtnListener();
 
 function subscriptionBtnListener() {
-  SUBSCRIPTION_BTN_SELECTOR.addEventListener('click', errorMsgBanner);
+  SUBSCRIPTION_BTN_SELECTOR.addEventListener('click', errorSubscriptionMsg);
 }
 
-function errorMsgBanner() {
+function errorSubscriptionMsg() {
   const SECTION_SELECTOR = document.querySelector("#cat-domination-plan");
-  const ERROR_MSG = document.createElement('p');
-  ERROR_MSG.textContent = '¡Lo sentimos! Algo salió mal.';
-  ERROR_MSG.classList.add('error');
+  const ERROR_MSG = document.createElement("p");
+  ERROR_MSG.textContent = "¡Lo sentimos! Algo salió mal.";
+  ERROR_MSG.classList.add("error");
   SECTION_SELECTOR.append(ERROR_MSG);
   setTimeout(() => {
     ERROR_MSG.remove();
-  }, 3000)
+  }, 3000);
+}
+
+function errorNewsletterMsg () {
+  const NEWSLETTER_ID_SELECTOR = document.querySelector("#newsletterSectionId");
+  const ERROR_MSG = document.createElement("p");
+  ERROR_MSG.textContent = "No puedes enviar el formulario con campos vacíos";
+  ERROR_MSG.classList.add("error");
+  NEWSLETTER_ID_SELECTOR.append(ERROR_MSG);
+  setTimeout(() => {
+   ERROR_MSG.remove();
+  }, 3000);
 }
 
 
